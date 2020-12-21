bc4b580f9c4fc121d2dd0394b7dd705ea9584d52
Philipp Schaedle <philipp.schaedle@gmail.com>
2020-12-21 18:21:30 +0100

    add file header with author information to all files.

--- src/porousReservoirBase.py
+++ src/porousReservoirBase.py
@@ -1,3 +1,18 @@
+# Licensed under LGPL 2.1, please see LICENSE for details
+# https://www.gnu.org/licenses/lgpl-2.1.html
+#
+# The work on this project has been performed at the GEG Group at ETH Zurich:
+# --> https://geg.ethz.ch
+#
+# The initial version of this file has been implemented by:
+#
+#     Philipp Schaedle (https://github.com/philippschaedle)
+#     Benjamin M. Adams
+#
+# Further changes are done by:
+#
+
+############################
 
 from utils.fluidState import FluidState
 
